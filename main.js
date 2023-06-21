251b4c2e54e25e10fbe5e26d0004ddf44fdc2c7a
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-06-21 11:28:51 +0500

    lesson3

---
+++ main.js
@@ -0,0 +1,202 @@
+// switch case
+
+// Homework
+
+// 1 misol
+
+
+// let haftaKunlari = 3;
+
+// switch (haftaKunlari) {
+//     case 1:
+//         console.log('Dushanba');
+//         break;
+//     case 2:
+//         console.log('Seshanba');
+//         break;
+//     case 3:
+//         console.log('Chorshanba');
+//         break;
+//     case 4:
+//         console.log('Payshanba');
+//         break;
+//     case 5:
+//         console.log('Juma');
+//         break;
+//     case 6:
+//         console.log('Shanba');
+//         break;
+//     case 7:
+//         console.log('Yakshanba');
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+// 2 misol
+
+// let k = 2;
+
+// switch (k) {
+//     case 1:
+//         console.log('yomon');
+//         break;
+//     case 2:
+//         console.log('qoniqarsiz');
+//         break;
+//     case 3:
+//         console.log('qoniqarli');
+//         break;
+//     case 4:
+//         console.log('yaxshi');
+//         break;
+//     case 5:
+//         console.log('alo');
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+
+// 3 misol
+
+// let oy = 11;
+
+// switch (oy) {
+//     case 1:
+//         console.log('qish');
+//         break;
+//     case 2:
+//         console.log('qish');
+//         break;
+//     case 3:
+//         console.log('bahor');
+//         break;
+//     case 4:
+//         console.log('bahor');
+//         break;
+//     case 5:
+//         console.log('bahor');
+//         break;
+//     case 6:
+//         console.log('yoz');
+//         break;
+//     case 7:
+//         console.log('yoz');
+//         break;
+//     case 8:
+//         console.log('yoz');
+//         break;
+//     case 9:
+//         console.log('kuz');
+//         break;
+//     case 10:
+//         console.log('kuz');
+//         break;
+//     case 11:
+//         console.log('kuz');
+//         break;
+//     case 12:
+//         console.log('qish');
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+
+// 4 misol
+
+// let oyRaqami = 4;
+
+// switch (oyRaqami) {
+//     case 1:
+//         console.log(31);
+//         break;
+//     case 2:
+//         console.log(28);
+//         break;
+//     case 3:
+//         console.log(31);
+//         break;
+//     case 4:
+//         console.log(30);
+//         break;
+//     case 5:
+//         console.log(31);
+//         break;
+//     case 6:
+//         console.log(30);
+//         break;
+//     case 7:
+//         console.log(31);
+//         break;
+//     case 8:
+//         console.log(31);
+//         break;
+//     case 9:
+//         console.log(30);
+//         break;
+//     case 10:
+//         console.log(31);
+//         break;
+//     case 11:
+//         console.log(30);
+//         break;
+//     case 12:
+//         console.log(31);
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+// 5 misol
+
+
+// let a = 4;
+// let b = 6;
+
+// let c = a + b;
+
+// switch (c) {
+//     case a * b:
+//         console.log(c);
+//         break;
+//     case a / b:
+//         console.log(c);
+//         break;
+//     case a - b:
+//         console.log(c);
+//         break;
+//     case a + b:
+//         console.log(c);
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+// 6 misol
+
+// let stependiya = 5;
+
+// switch (stependiya) {
+//     case 2:
+//         console.log('stependiya berilmidi');
+//         break;
+//     case 3:
+//         console.log('stependiya beriladi lekin miqdori kamroq');
+//         break;
+//     case 4:
+//         console.log('stependiya berilada lekin miqdori ortacha');
+//         break;
+//     case 5:
+//         console.log('stependiya beriladi lekin miqdori koproq');
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
